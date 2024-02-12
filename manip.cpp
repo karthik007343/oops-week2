99565a86efa364a49824b24be3495baeadba336e
karthik007343 <159223934+karthik007343@users.noreply.github.com>
2024-02-12 07:21:51 +0530

    Create manip.cpp

---
+++ manip.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float d=89.9056;
+    string n;
+    cout<<"hi "<<endl;
+    cout<<"world"<<ends;
+    cout<<setw(10)<<"karthik"<<endl;
+    cout<<setw(10)<<setfill('*')<<"karthik"<<endl;
+    cout<<setprecision(11)<<d<<endl;
+    cout<<"the world"<<flush<<endl;
+    cout.flush();
+    cout<<"enter name : ";
+    cin>>ws;
+    cin>>n;
+    cout<<"the name is "<<n<<endl;
+}
